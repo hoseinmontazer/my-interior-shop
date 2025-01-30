0ad360af179f1a48664dfa5d40e95eea94d50c9f
javadi <javadi89s@gmail.com>
2025-01-30 12:49:33 +0330

    input & output deleted

--- src/components/contact/Address.tsx
+++ src/components/contact/Address.tsx
@@ -3,53 +3,36 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { MdOutlineAccessTimeFilled } from "react-icons/md";
 
 const Address = () => {
-    return (
-            <div className="w-full xl:w-1/2 xl:pl-36 xl:py-4">
-                <div className="flex p-3">
-                    <div>
-                        <IoLocationSharp className="text-2xl mr-4" />
-                    </div>
-                    <div className="p-1">
-                        <div className="text-2xl font-semibold">Address</div>
-                        <div className="w-1/2">
-                            236 5th SE Avenue, New York NY10000, United States
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex p-3">
-                    <div>
-                        <FaPhoneAlt className="text-2xl mr-4" />
-                    </div>
-                    <div className="p-1">
-                        <div className="text-2xl font-semibold">Phone</div>
-                        <div className="">
-                            <div>
-                                Mobile:+(1) 546-6789
-                            </div>
-                            <div>
-                                Mobile:+(1) 456-6789
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex p-3">
-                    <div>
-                        <MdOutlineAccessTimeFilled className="text-2xl mr-4" />
-                    </div>
-                    <div className="p-1">
-                        <div className="text-2xl font-semibold">Working Time</div>
-                        <div>
-                            Monday-Friday: 9:00 - 22:00
-                        </div>
-                        <div>
-                            Saturday-Sunday: 9:00 - 21:00
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-    )
+  return (
+    <main className="w-full xl:w-1/2 xl:pl-36 xl:py-4">
+      <section className="flex p-3">
+        <IoLocationSharp className="size-10 mr-4" />
+        <div className="p-1">
+          <h2 className="text-2xl font-semibold">Address</h2>
+          <div className="w-1/2">
+            236 5th SE Avenue, New York NY10000, United States
+          </div>
+        </div>
+      </section>
+      <section className="flex p-3">
+        <FaPhoneAlt className="size-10 mr-4" />
+        <div className="p-1">
+          <h2 className="text-2xl font-semibold">Phone</h2>
+          <ul className="">
+            <li>Mobile:+(1) 546-6789</li>
+            <li>Mobile:+(1) 456-6789</li>
+          </ul>
+        </div>
+      </section>
+      <section className="flex p-3">
+        <MdOutlineAccessTimeFilled className="size-10 mr-4" />
+        <div className="p-1">
+          <h2 className="text-2xl font-semibold">Working Time</h2>
+          <div>Monday-Friday: 9:00 - 22:00</div>
+          <div>Saturday-Sunday: 9:00 - 21:00</div>
+        </div>
+      </section>
+    </main>
+  );
 };
-export default Address
+export default Address;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,17 +4,17 @@ import UpperFooter from "../components/footer/UpperFooter";
 import MyHeader from "../components/header/MyHeader";
 import MySecHeader from "../components/header/MySecHeader";
 
-const Contact=()=>{
-    return(
-        <div className="bg-black">
-            <div className="bg-white max-w-[1400px] mx-auto">
-                <MyHeader />
-                <MySecHeader title="Contact"/>
-                <ContactInfo />
-                <UpperFooter />
-                <Footer />
-            </div>
-        </div>
-    )
+const Contact = () => {
+  return (
+    <div className="bg-black">
+      <div className="shop-container">
+        <MyHeader />
+        <MySecHeader title="Contact" />
+        <ContactInfo />
+        <UpperFooter />
+        <Footer />
+      </div>
+    </div>
+  );
 };
-export default Contact
+export default Contact;
